f7299dd8ed8a6d46606e96f554de99eea9f0681f
ferdie bergado <ferdiebergado@gmail.com>
2026-06-30 21:20:58 +0800

    refactor: Lazy-load RequireUser

--- src/page.tsx
+++ src/page.tsx
@@ -1,9 +1,9 @@
 import { lazy } from 'react';
 import { Route, Switch } from 'wouter';
 import RequireGuest from './auth/components/require-guest';
-import RequireUser from './auth/components/require-user';
 import SigninPage from './pages/signin-page';
 
+const RequireUser = lazy(() => import('./auth/components/require-user'));
 const Dashboard = lazy(() => import('./pages/dashboard'));
 const NotFound = lazy(() => import('./pages/not-found'));
 
