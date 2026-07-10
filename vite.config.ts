bfe9b748ff818cab885602c3e70a2317b82e3984
ferdie bergado <ferdiebergado@gmail.com>
2026-07-10 16:01:48 +0800

    chore: Setup react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
+import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(import.meta.dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      isProduction &&
+        babel({
+          presets: [reactCompilerPreset()],
+        }),
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        '@': path.resolve(import.meta.dirname, './src'),
+      },
     },
-  },
+  };
 });
