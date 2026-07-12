e002bec4a78ad8a8140463a8ec99bc70adeb2599
ferdie bergado <ferdiebergado@gmail.com>
2026-07-12 10:13:33 +0800

    chore: Setup vitest browser mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
+/// <reference types="vitest/config" />
 import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import { playwright } from '@vitest/browser-playwright';
 import path from 'path';
 import { defineConfig } from 'vite';
 
@@ -20,6 +22,29 @@ export default defineConfig(({ mode }) => {
       alias: {
         '@': path.resolve(import.meta.dirname, './src'),
       },
+    },
+    test: {
+      projects: [
+        {
+          test: {
+            name: 'unit',
+            include: ['./**/*.{test,spec}.ts'],
+            environment: 'node',
+          },
+        },
+        {
+          test: {
+            name: 'browser',
+            include: ['./**/*.{test,spec}.tsx'],
+            browser: {
+              enabled: true,
+              provider: playwright(),
+              instances: [{ browser: 'chromium' }],
+              headless: true,
+            },
+          },
+        },
+      ],
     },
   };
 });
