8c9155a35dbe90c7a91a35c404ef02abca426291
ferdie bergado <ferdiebergado@gmail.com>
2026-07-17 15:05:09 +0800

    feat: Add textarea field component

---
+++ src/components/textarea-field.tsx
@@ -0,0 +1,18 @@
+import type { ComponentProps } from 'react';
+import type { FieldValues } from 'react-hook-form';
+import type { FormFieldProps } from './form-field';
+import FormField from './form-field';
+import { Textarea } from './ui/textarea';
+
+export default function TextareaField<TFieldValues extends FieldValues>(
+  props: FormFieldProps<TFieldValues> &
+    Omit<ComponentProps<'textarea'>, 'className'>,
+) {
+  const { name, label, control, ...textAreaProps } = props;
+
+  return (
+    <FormField name={name} label={label} control={control}>
+      {({ field }) => <Textarea {...field} {...textAreaProps} />}
+    </FormField>
+  );
+}
