25ccf8956ba13add55d7571f3fc4e90fb5138ae4
ferdie bergado <ferdiebergado@gmail.com>
2026-07-17 15:04:56 +0800

    feat: Add input field component

---
+++ src/components/input-field.tsx
@@ -0,0 +1,18 @@
+import type { ComponentProps } from 'react';
+import type { FieldValues } from 'react-hook-form';
+import type { FormFieldProps } from './form-field';
+import FormField from './form-field';
+import { Input } from './ui/input';
+
+export default function InputField<TFieldValues extends FieldValues>(
+  props: FormFieldProps<TFieldValues> &
+    Omit<ComponentProps<'input'>, 'className'>,
+) {
+  const { name, label, control, ...inputProps } = props;
+
+  return (
+    <FormField name={name} label={label} control={control}>
+      {({ field }) => <Input {...field} {...inputProps} />}
+    </FormField>
+  );
+}
